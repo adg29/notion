c26ac03addef6f76fac4bc0726ba9ed3e96dfb8c
Alan Garcia <alandavidgarcia@gmail.com>
2020-12-29 12:54:09 -0500

    Update loom-video-embed-currentTime.js

--- loom-video-embed-currentTime.js
+++ loom-video-embed-currentTime.js
@@ -1,9 +1,11 @@
 player = document.querySelector('.video-player-video');
-$('header button:contains("Share")').click();
-$('.share-video-embed-code-dropdown-item').click();
-embed = $('.embed-code').text()
-$embed = $(embed)
-src = $embed.find('iframe').attr('src')
-$embed.find('iframe').attr('src',src+'?t='+player.currentTime)
-embedUpdate = $('.embed-code').text()
+jQuery('header button:contains("Share")').click();
+jQuery('.share-video-embed-code-dropdown-item').click();
+jQuery('.embed-show-code-button').click();
+embed = jQuery('.embed-code').text();
+$embed = jQuery(embed);
+src = $embed.find('iframe').attr('src');
+$embed.find('iframe').attr('src', src+'?t='+player.currentTime);
+embedUpdate = $embed.text();
+embedUpdate = jQuery('.embed-code').text();
 t=embedUpdate;d=document;d.body.appendChild(Object.assign(d.createElement('textarea'),{value:t})).select();d.execCommand('copy');
